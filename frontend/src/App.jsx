382c4a05f7737692837c16efb0a80dd2112e1b40
StellaNDL <157975414+StellaNDL@users.noreply.github.com>
2025-12-03 13:38:37 +0200

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReservationForm from './Components/ReservationForm'
 import { ToastContainer } from 'react-toastify'
 
-export const backendUrl ="https://stellar-backend-zp1l.onrender.com"
+export const backendUrl ="https://stellar-backend2.onrender.com"
 
 const App = () => {
   return (

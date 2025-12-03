52d24b1e24da147cb7ab4b05e6f0c4ed3bbce505
StellaNDL <157975414+StellaNDL@users.noreply.github.com>
2025-12-03 12:42:23 +0200

    Update reservationRoute.js

--- backend/routes/reservationRoute.js
+++ backend/routes/reservationRoute.js
@@ -5,6 +5,7 @@ const router = express.Router();
 
 router.post('/create', createReservation);
 router.get('/get', getAllReservation)
-router.delete('/delete:id', deleteReservation)
+router.delete('/delete/:id', deleteReservation)
+
 
 export default router;
